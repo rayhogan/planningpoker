fe2b61dc372f3b61816c660c3e4cc3a662265e07
Ray Hogan <rayhogan@gmail.com>
2020-10-10 13:34:20 +0100

    Added ability to edit story title.

--- public/scripts/engine.js
+++ public/scripts/engine.js
@@ -24,12 +24,34 @@ this.socket.on('disconnect', function (userId) {
 });
 
 // Add the users to the webpage
-function RenderUsers()
-{
+function RenderUsers() {
     var userMarkup = '';
     Object.keys(connectedUsers).forEach(function (id) {
-        userMarkup += '<div> User '+connectedUsers[id].userId+' </div>';
-    }); 
+        userMarkup += '<div> User ' + connectedUsers[id].userId + ' </div>';
+    });
 
     document.getElementById('UserList').innerHTML = userMarkup;
 }
+
+// Apply update if a connected user updates the story title
+this.socket.on('updateStory', function (storyInfo) {
+    document.getElementById('storyDisplay').innerText = storyInfo;
+});
+
+
+function editFinished() {
+    document.getElementById('storyDisplay').innerText = document.getElementById('storyTitle').value;
+    document.getElementById('storyTitle').style.display = "none";
+    document.getElementById('storyDisplay').style.display = "block";
+
+    // Push update back to server
+    this.socket.emit('storyUpdatedByUser', document.getElementById('storyTitle').value);
+}
+
+document.getElementById('storyDisplay')
+    .addEventListener('click', function (event) {
+        document.getElementById('storyTitle').value = document.getElementById('storyDisplay').innerText;
+        document.getElementById('storyDisplay').style.display = "none";
+        document.getElementById('storyTitle').style.display = "block";
+        document.getElementById('storyTitle').focus();
+    });
