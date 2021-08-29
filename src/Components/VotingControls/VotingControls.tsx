f4a6efbc5aec3458c39f2f9ed292baafb347bc08
Ray Hogan <rayhogan@gmail.com>
2021-08-29 20:56:30 +0100

    Cleaned up imports

--- src/Components/VotingControls/VotingControls.tsx
+++ src/Components/VotingControls/VotingControls.tsx
@@ -1,7 +1,5 @@
-import { StoreMallDirectory } from '@material-ui/icons';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
-import { makeStyles } from '@material-ui/core/styles';
 import React from 'react';
 import './VotingControls.css';
 
