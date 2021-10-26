8b11e332ee6343350451a11687f03573f437e904
Ray Hogan <rayhogan@gmail.com>
2021-10-26 19:30:23 +0100

    Removed grid + added component skeleton

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -2,6 +2,7 @@ import './App.css';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import Login from './Login/Login';
+import StoryBacklog from './StoryBacklog/StoryBacklog';
 import ActiveStory from './ActiveStory/ActiveStory';
 import VotingControls from './VotingControls/VotingControls';
 import AdminControls from './AdminControls/AdminControls';
@@ -35,7 +36,7 @@ function App() {
               <ConnectedUsers />
             </Grid>
             <Grid item xs={9}>
-              <Paper>Empty Space</Paper>
+              <StoryBacklog/>
             </Grid>
           </Grid>
         </div>
